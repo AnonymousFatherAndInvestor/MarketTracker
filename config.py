abf85c5eb05d3ab2733eb680f060050c54f70664
AnonymousFatherAndInvestor <anonymousinvestor.sg@gmail.com>
2025-05-23 13:20:45 +0900

    Handle empty data, expand tickers

---
+++ config.py
@@ -0,0 +1,12 @@
+TICKERS = [
+    "^GSPC", "^IXIC", "^RUT",
+    "^FTSE", "^STOXX50E",
+    "^N225", "^TOPX", "9816.T", "1343.T",
+    "^HSI", "000001.SS", "^STI",
+    "^IRX", "^TNX", "^JGB10Y", "^SG10Y=RR", "^DE10Y=X",
+    "JPY=X", "EURJPY=X", "GBPJPY=X", "SGDJPY=X", "CHFJPY=X",
+    "IXN", "IXG", "IXJ", "IXC", "MXI", "EXI", "RXI", "KXI", "IXP", "JXI", "RWO",
+    "IWVL.L", "IWFG.L", "IWFM.L", "IWQU.L", "ACWV"
+]
+
+REFRESH_INTERVAL = 900  # seconds
